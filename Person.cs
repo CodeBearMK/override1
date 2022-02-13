5e7256f8ec97eee7ac13d2b41897796c439422f8
cat32tw <mike1993xd@gmail.com>
2022-02-13 21:44:54 +0800

    子類別覆寫父類別練習

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace override1
+{
+    internal class Person:Animal
+    {
+        public new string Mammal
+        {
+            get { return "人"; }
+        }
+
+        public string Special
+        {
+            get { return "智慧"; }
+        }
+
+        public override void display()
+        {
+            Console.WriteLine("{0} 是 {1}", Mammal, kind);
+            Console.WriteLine("{0} 是 {1} 的 {2}", Mammal, Special, kind);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace override1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog  = new Animal();
+            dog.Mammal = "狗";
+            dog.display();
+            Console.WriteLine("--------------------");
+            Person per = new Person();
+            per.display();
+            Console.Read();
+        }
+    }
+}
